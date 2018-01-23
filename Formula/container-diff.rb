c803d4c2c22b579f10848d7fdc0e6a8ba8299fb4
ilovezfs <ilovezfs@icloud.com>
2018-01-23 08:33:07 -0800

    Update container-diff.rb

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -1,8 +1,8 @@
 class ContainerDiff < Formula
   desc "Diff your Docker containers"
   homepage "https://github.com/GoogleCloudPlatform/container-diff"
-  url "https://github.com/GoogleCloudPlatform/container-diff/archive/v0.5.2.tar.gz"
-  sha256 "9f64267b01e9f96efd053eadbac6435bdb15c05597e8ebe4e7081e13fb7e45cf"
+  url "https://github.com/GoogleCloudPlatform/container-diff/archive/v0.6.2.tar.gz"
+  sha256 "a3c680799c230d2a2352eb1e5765bd6774182b213a73e5c0bf1e6254008cd434"
 
   depends_on "go" => :build
 
